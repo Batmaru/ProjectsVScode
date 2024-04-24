e20f0bd3d982b7a445b1cf7ae60d8161826b2bb9
Batmaru <marwanrafik02@gmail.com>
2024-04-25 00:29:26 +0200

    esercizi compito esercitazione(funzioni con Bardh)

---
+++ esercizi_per_casa/esercizi_lezione4_valutazione.py
@@ -0,0 +1,62 @@
+#Sei un detective esperto nel mondo dei testi e un caso sconcertante è arrivato sulla tua scrivania. Due stringhe, s e t, sono i 
+# tuoi sospettati. La tua missione: determinare se s si nasconde in bella vista all'interno di t, ma con una svolta!
+
+#Qual è il problema?
+#Non puoi semplicemente confrontare le stringhe lettera per lettera. Immagina che s sia un personaggio subdolo che 
+#cerca di confondersi tra la folla (t). Potrebbero camuffarsi nascondendosi tra altri personaggi, ma non cambiano mai 
+#il loro ordine! Quindi, "ace" può intrufolarsi in "abcde" (rimuove semplicemente "b" e "d"), ma "aec" non funzionerebbe 
+#(l'ordine cambia).
+#Scrivi una funzione di interruzione del codice che prenda la stringa s (il carattere subdolo) e t (la folla) come input. 
+#Se è possibile trovare s in agguato all'interno di t mantenendo il suo travestimento (ordine), restituisce True. Altrimenti 
+#restituisce False. Dimostra le tue abilità da detective e svela la verità nascosta!2
+def is_subsequence(s: str, t: str) -> bool:
+    cont_s=0
+    cont_t=0
+    while cont_t<len(t) and cont_s<len(s):
+        if s[cont_s] == t[cont_t]:
+            cont_s+=1
+        cont_t+=1
+    return cont_s==len(s)
+    
+
+print(is_subsequence("abc", "ahbgdc"))
+print(is_subsequence("axc", "ahbgdc"))
+print(is_subsequence("cba",'fdidfavjocdfionab'))
+print()
+
+#Immagina di avere uno scrigno pieno di gioielli (rappresentati da una lista di numeri interi). Questi gioielli hanno 
+# vari valori, alcuni più preziosi di altri. Il tuo compito è trovare il terzo gioiello distinto più prezioso nello scrigno.
+#Qual è la svolta?
+#Nello scrigno possono esserci gioielli duplicati (numeri con lo stesso valore). A noi però interessano solo valori distinti,
+# ovvero gioielli con valori unici.
+#Scrivi una funzione che prenda come input questo array di valori di gioielli (numeri). Se nello scrigno sono presenti 
+# almeno tre valori distinti, la funzione dovrebbe restituire il valore del terzo gioiello distinto di maggior valore.
+#Ma c'è un problema!
+#Se non ci sono tre valori distinti (ad esempio, solo due valori univoci o tutti i valori sono uguali), 
+# la funzione dovrebbe restituire il valore del gioiello più prezioso nello scrigno.
+
+def third_max(nums: list[int]) -> int:
+    i=0
+    if not nums:
+        return None
+    l_no_multiples: list=[]
+    for n in nums:
+        if n not in l_no_multiples:
+            l_no_multiples.append(n)
+    if len(l_no_multiples)>=3:
+        return l_no_multiples[2]
+    else:
+        return max(l_no_multiples)
+
+print(third_max([3, 2, 1]))
+print(third_max([1, 2]))
+print(third_max([2, 2, 3, 1]))
+print(third_max([4, 5, 5, 6, 6, 6]))   
+print(third_max([1, 2, 3, 4, 5]))      
+print(third_max([5, 5, 5, 5, 5]))      
+print(third_max([1, 1, 2, 2, 3, 3]))  
+print(third_max([1, 2, 3, 4, 4, 4]))   
+print(third_max([2, 2, 2, 2, 2, 2]))   
+print(third_max([1]))                  
+print(third_max([]))                   
+
