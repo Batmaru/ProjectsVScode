b2f5bfc20fb9ac4a0656335c00692511ee09b811
Batmaru <marwanrafik02@gmail.com>
2024-07-15 12:52:38 +0200

    codificatore

--- esercizi/lezione24/codificatore.py
+++ esercizi/lezione24/codificatore.py
@@ -20,19 +20,18 @@ class CifratoreAScorrimento(CodificatoreMessaggi, DecodificatoreMessaggio):
         alfabetostr = 'abcdefghijklmnopqrstuvwxyz'
         testoinchiaro=testoinchiaro.lower()
         listacodifica = []
-        
+      
+        while self.chiave>len(alfabetostr):
+            self.chiave=self.chiave-len(alfabetostr)      
         for element in testoinchiaro:
             if element in alfabetostr:
                 elementalfindex = alfabetostr.index(element)
                 new_index = (elementalfindex + self.chiave) 
-                if new_index<=len(alfabetostr):
+                if new_index<len(alfabetostr):
                     listacodifica.append(alfabetostr[new_index])
                 else:
                     new_index=new_index-len(alfabetostr)
                     listacodifica.append(alfabetostr[new_index])
-
-            else:
-                listacodifica.append(element)
                 
         
         return ''.join(listacodifica)
@@ -59,29 +58,35 @@ class CifratoreAScorrimento(CodificatoreMessaggi, DecodificatoreMessaggio):
         
         return ''.join(listacodifica)
 
-class Cifratoreacombinazione(CodificatoreMessaggi):
+class CifratoreACombinazione(CodificatoreMessaggi, DecodificatoreMessaggio):
     def __init__(self, numero: int):
         self.numero=numero
 
     def codifica(self, testoinchiaro:str):
         testoinchiaro=testoinchiaro.lower()
+        lentestoinchiaro=len(testoinchiaro)
         numerocicli=1
+        tuttelecodifiche: list=[]
         while numerocicli<=self.numero:
             if len(testoinchiaro)%2!=0:
                 lunghezzastringacorta=len(testoinchiaro)//2
                 lunghezzastringalunga=lunghezzastringacorta+1
                 stringalunga=''
                 stringacorta=''
+                
                 for n in range(0,lunghezzastringalunga):
                     stringalunga+=testoinchiaro[n]
                 for n in range(lunghezzastringalunga,len(testoinchiaro)):
                     stringacorta+=testoinchiaro[n]
+                testoinchiaro=''
                 for n in range(0,lunghezzastringacorta):
-                    testoinchiaro=''+ lunghezzastringalunga[n]
-                    testoinchiaro+= lunghezzastringacorta[n]
-                    if n==len(lunghezzastringacorta):
-                        testoinchiaro+= lunghezzastringalunga[n+1]
-                return testoinchiaro
+                    testoinchiaro+= stringalunga[n]
+                    testoinchiaro+= stringacorta[n]
+                    if len(testoinchiaro)==lentestoinchiaro-1:
+                        testoinchiaro+= stringalunga[n+1]
+                        tuttelecodifiche.append(testoinchiaro)
+                        numerocicli+=1
+                        
             else:
                 lunghezzastringa=len(testoinchiaro)//2
                 stringa1=''
@@ -93,8 +98,33 @@ class Cifratoreacombinazione(CodificatoreMessaggi):
                 for n in range(0,lunghezzastringa):
                     testoinchiaro=''+stringa1[n]
                     testoinchiaro+= stringa2[n]
-                return testoinchiaro
+                    tuttelecodifiche.append(testoinchiaro)
+                    numerocicli+=1
+        return f'le codifiche sono: {tuttelecodifiche}, quella finale è {testoinchiaro}'
+    
+    def decodifica(self, testoinchiaro: str):
+        testoinchiaro=testoinchiaro.lower()
+        numerocicli=1
+        tuttelecodifiche: list=[]
+        while numerocicli<=self.numero:
+    
+            lunghezzastringa=len(testoinchiaro)
+            stringa1=''
+            stringa2=''
+            for n in range(0,lunghezzastringa):
+                if n%2==0:
+                    stringa1+=testoinchiaro[n]
+                else:
+                    stringa2+=testoinchiaro[n]
+            testoinchiaro=stringa1+stringa2
+            tuttelecodifiche.append(testoinchiaro)
+            numerocicli+=1
+        return f'le codifiche sono: {tuttelecodifiche}, quella finale è {testoinchiaro}'
+
+
                 
+                    
+
 
 
 
@@ -110,11 +140,12 @@ class Cifratoreacombinazione(CodificatoreMessaggi):
 
     
 
-chiave1=CifratoreAScorrimento(5)
+chiave1=CifratoreAScorrimento(2)
 
-print(chiave1.codifica('ciaoZ'))
-print(chiave1.decodifica('abctG'))
+print(chiave1.codifica('abcdefghijklmnopqrstuvwxyz'))
+print(chiave1.decodifica('cdefghijklmnopqrstuvwxyzab'))
 
-numero1=Cifratoreacombinazione(3)
+numero1=CifratoreACombinazione(4)
 
 print(numero1.codifica('asdrubale'))
+print(numero1.decodifica('auerldasb'))
